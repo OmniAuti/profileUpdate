8928d5776425bfef0dc3fc8f2769e71dd2d2217e
Austin Olijar <Austin.Olijar@gmail.com>
2021-12-03 17:39:21 -0600

    Add files via upload

--- profile.js
+++ profile.js
@@ -3,15 +3,54 @@ const particleCont = document.getElementById("particles-js");
 const contactCont = document.querySelector(".work-together");
 const contactCard = document.querySelector(".stuck");
 const contactCardOn = document.querySelector(".stuck-on");
-const contactCardTwo = document.querySelector(".stack");
-const contactCardThree = document.querySelector(".stacked");
+const designCard = document.querySelector(".stack");
+const codeCard = document.querySelector(".stacked");
 
 function openingNight() {
   card.forEach((card) => {
     card.addEventListener("click", () => {
       card.classList.toggle("theater");
-      contactCard.classList.add('curtain-closed')
+
+      contactCard.classList.add("curtain-closed");
       card.classList.add("curtain-closed");
+
+      if (designCard.classList.contains("theater")) {
+        contactCard.classList.add("move-card-back");
+        contactCardOn.classList.add("move-card-back");
+        codeCard.classList.add("move-card-back");
+      } else if (designCard.classList.contains("curtain-closed")) {
+        contactCard.classList.remove("move-card-back");
+        contactCardOn.classList.remove("move-card-back");
+        codeCard.classList.remove("move-card-back");
+        contactCard.classList.add("move-card-forward");
+        contactCardOn.classList.add("move-card-forward");
+        codeCard.classList.add("move-card-forward");
+
+        setTimeout(() => {
+          contactCard.classList.remove("move-card-forward");
+          contactCardOn.classList.remove("move-card-forward");
+          codeCard.classList.remove("move-card-forward");
+        }, 1000);
+      }
+
+      if (codeCard.classList.contains("theater")) {
+        contactCard.classList.add("move-card-back");
+        contactCardOn.classList.add("move-card-back");
+        designCard.classList.add("move-card-back");
+      } else if (codeCard.classList.contains("curtain-closed")) {
+        contactCard.classList.remove("move-card-back");
+        contactCardOn.classList.remove("move-card-back");
+        designCard.classList.remove("move-card-back");
+        contactCard.classList.add("move-card-forward");
+        contactCardOn.classList.add("move-card-forward");
+        designCard.classList.add("move-card-forward");
+
+        setTimeout(() => {
+          contactCard.classList.remove("move-card-forward");
+          contactCardOn.classList.remove("move-card-forward");
+          designCard.classList.remove("move-card-forward");
+        }, 1000);
+      }
 
       setTimeout(() => {
         card.classList.remove("curtain-closed");
@@ -26,8 +65,8 @@ openingNight();
 contactCard.addEventListener("click", () => {
   contactCard.classList.remove("border-on");
   contactCardOn.classList.remove("border-on");
-  contactCardTwo.classList.remove("border-on");
-  contactCardThree.classList.remove("border-on");
+  designCard.classList.remove("border-on");
+  codeCard.classList.remove("border-on");
 
   setTimeout(() => {
     contactCard.style.zIndex = "-1";
@@ -45,8 +84,8 @@ contactCard.addEventListener("click", () => {
 contactCardOn.addEventListener("click", () => {
   contactCard.classList.add("border-on");
   contactCardOn.classList.add("border-on");
-  contactCardTwo.classList.add("border-on");
-  contactCardThree.classList.add("border-on");
+  designCard.classList.add("border-on");
+  codeCard.classList.add("border-on");
 
   contactCard.style.zIndex = "0";
   contactCardOn.style.zIndex = "-1";
