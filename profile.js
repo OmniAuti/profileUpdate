2da205b0337e4f95f74333467484a875520a6a49
Austin Olijar <Austin.Olijar@gmail.com>
2021-12-03 15:38:27 -0600

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,56 +1,57 @@
-const card = document.querySelectorAll('.portfolio-deck');
-
-
+const card = document.querySelectorAll(".portfolio-deck");
 
 function openingNight() {
-    card.forEach(card => {
-        card.addEventListener('click', () => {
-            card.classList.toggle('theater')
-            card.classList.add('curtain-closed')
-            setTimeout(() => {
-                card.classList.remove('curtain-closed')
-            }, 1000)
-        })
-    })
+  card.forEach((card) => {
+    card.addEventListener("click", () => {
+      card.classList.toggle("theater");
+      card.classList.add("curtain-closed");
+      setTimeout(() => {
+        card.classList.remove("curtain-closed");
+      }, 1000);
+    });
+  });
 }
 
-openingNight()
+openingNight();
 
+const particleCont = document.getElementById("particles-js");
+const contactCont = document.querySelector(".work-together");
+const contactCard = document.querySelector(".stuck");
+const contactCardOn = document.querySelector(".stuck-on");
+const contactCardTwo = document.querySelector(".stack");
+const contactCardThree = document.querySelector(".stacked");
 
+contactCard.addEventListener("click", () => {
+  contactCard.classList.remove("border-on");
+  contactCardOn.classList.remove("border-on");
+  contactCardTwo.classList.remove("border-on");
+  contactCardThree.classList.remove("border-on");
 
-const particleCont = document.getElementById('particles-js')
-const contactCont = document.querySelector('.work-together')
-const contactCard = document.querySelector('.stuck')
-const contactCardOn = document.querySelector('.stuck-on')
-const contactCardTwo = document.querySelector('.stack')
-const contactCardThree = document.querySelector('.stacked')
+  setTimeout(() => {
+    contactCard.style.zIndex = "-1";
+    contactCardOn.style.zIndex = "0";
+  }, 300);
 
-contactCard.addEventListener('click', () => {
-    contactCard.classList.remove('border-on');
-    contactCardOn.classList.remove('border-on');
-    contactCardTwo.classList.remove('border-on');
-    contactCardThree.classList.remove('border-on');
+  particleCont.style.top = "0";
+  contactCont.style.transform = 'translateY(0)'
+  contactCont.style.display = "block";
 
-    contactCard.style.zIndex = '-1'
-    contactCardOn.style.zIndex = '0'
+  window.scrollTo(0, document.body.scrollHeight);
+  document.body.style.overflow = "hidden";
+});
 
-    particleCont.style.top = '0';
-    contactCont.style.display = 'block';
-    
-    window.scrollTo(0,document.body.scrollHeight);
-    document.body.style.overflow = 'hidden'
-})
+contactCardOn.addEventListener("click", () => {
+  contactCard.classList.add("border-on");
+  contactCardOn.classList.add("border-on");
+  contactCardTwo.classList.add("border-on");
+  contactCardThree.classList.add("border-on");
 
-contactCardOn.addEventListener('click', () => {
-    contactCard.classList.add('border-on');
-    contactCardOn.classList.add('border-on');
-    contactCardTwo.classList.add('border-on');
-    contactCardThree.classList.add('border-on');
-
-    contactCard.style.zIndex = '0'
-    contactCardOn.style.zIndex = '-1'
-
-    particleCont.style.top = '-50vh';
-    contactCont.style.display = 'none';
-    document.body.style.overflow = 'scroll'
-})
+  contactCard.style.zIndex = "0";
+  contactCardOn.style.zIndex = "-1";
+  contactCont.style.transform = 'translateY(50vh)'
+  setTimeout(() => {
+    particleCont.style.top = "-50vh";
+    contactCont.style.display = "none";
+  }, 300);
+  document.body.style.overflow = "scroll";
+});
