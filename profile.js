5ffc44116e16f11c22812ebe2857f89fd96b60d3
Austin Olijar <Austin.Olijar@gmail.com>
2021-12-06 09:36:50 -0600

    Add files via upload

--- profile.js
+++ profile.js
@@ -1,4 +1,3 @@
-const card = document.querySelectorAll(".portfolio-deck");
 const contactCont = document.querySelector(".work-together");
 const portfolioCont = document.querySelector(".portfolio-cont");
 const contactCard = document.querySelector(".stuck");
@@ -9,89 +8,140 @@ const codeCard = document.querySelector(".stacked");
 const codeLink = document.querySelector(".code-link");
 const aboutMeTitle = document.querySelector(".about-me-title");
 const derp = document.querySelector(".derp");
-const fullVH = window.innerHeight; // RESET HEIGHT FOR PORTFOLIO CARDS AS THEY TRANSITION
 
 // HANDLES OPENING CARD ANIMATIONS FOR CODE, DESIGN, CONTACT SECTIONS
-function openingContact() {
-  card.forEach((card) => {
-    card.addEventListener("click", () => {
 
-      if (contactCard.classList.contains("active-contact")) {
-        designLink.style.opacity = 0
-        codeLink.style.opacity = 0
-        handleContact()
-        setTimeout(() => {
-          designLink.style.opacity = 1
-          codeLink.style.opacity = 1
-        }, 1000)
-      }
+const designCloseBtn = document.querySelector(".design-close-btn");
+const codeCloseBtn = document.querySelector(".code-close-btn");
 
-      card.classList.toggle("theater"); // ACTIVE ON SCREEN FRONT AND CENTER
-
-      contactCard.classList.add("curtain-closed"); // MAKES OTHER ELEMENTS UNCLICKABLE
-      card.classList.add("curtain-closed"); // MAKES OTHER ELEMENTS UNCLICKABLE
-
-      if (designCard.classList.contains("theater")) {
-        aboutMeTitle.style.zIndex = -1;
-        designCard.style.height = null; // WIPES ELEMENT.STYLE SETTINGS
-        designCard.style.width = null;
-        designCard.style.left = null;
-        designCard.style.top = null;
-        contactCard.classList.add("move-card-back"); // ANIMATION TO MOVE UNUSED CARDS OUT OF VIEW
-        codeCard.classList.add("move-card-back");
-        setScrollOnOpenTheater();
-      } else if (designCard.classList.contains("curtain-closed")) {
-        setHieghts();
-        window.scrollTo({ top: window.innerHeight, left: 0, behavior: "smooth" }); // FORCED SCROLL TO CONTACT SECTION
-        contactCard.classList.remove("move-card-back");
-        codeCard.classList.remove("move-card-back");
-        contactCard.classList.add("move-card-forward"); // ANIMATION TO MOVE UNUSED CARDS INTO VIEW AS CURRENT SELECTION CLOSES
-        codeCard.classList.add("move-card-forward");
-        setTimeout(() => {
-          aboutMeTitle.style.zIndex = 1;
-          document.body.style.overflow = "scroll"; // RESETS LOCKED SCROLL
-          document.body.style.overflowX = "hidden";
-          // REMOVES ANIMATION AS ANIMATION COMPLETES FORWARD
-          contactCard.classList.remove("move-card-forward");
-          codeCard.classList.remove("move-card-forward");
-        }, 1000);
-      }
-
-      if (codeCard.classList.contains("theater")) {
-        aboutMeTitle.style.zIndex = -1;
-        // REPEATS SAME MOTIONS BUT FOR CODE CARD
-        codeCard.style.height = null; // WIPES ELEMENT.STYLE SETTINGS
-        codeCard.style.width = null;
-        codeCard.style.left = null;
-        codeCard.style.top = null;
-        contactCard.classList.add("move-card-back");
-        designCard.classList.add("move-card-back");
-        setScrollOnOpenTheater();
-      } else if (codeCard.classList.contains("curtain-closed")) {
-        setHieghts();
-        window.scrollTo({ top: window.innerHeight, left: 0, behavior: "smooth" }); // FORCED SCROLL TO CONTACT SECTION
-        contactCard.classList.remove("move-card-back");
-        designCard.classList.remove("move-card-back");
-        contactCard.classList.add("move-card-forward");
-        designCard.classList.add("move-card-forward");
-        setTimeout(() => {
-          aboutMeTitle.style.zIndex = 1;
-          document.body.style.overflow = "scroll"; // RESETS LOCKED SCROLL
-          document.body.style.overflowX = "hidden";
-          contactCard.classList.remove("move-card-forward");
-          designCard.classList.remove("move-card-forward");
-        }, 1000);
-      }
-      // REMOVES UNCLICKABLE CLASS
-      setTimeout(() => {
-        card.classList.remove("curtain-closed");
-        contactCard.classList.remove("curtain-closed");
-      }, 1000);
-    });
-  });
+function handleDesignCloseBtn() {
+  if (designCard.classList.contains("theater")) {
+    //CHECK IF ACTIVE CARD
+    designCard.classList.add("close-theater"); // ADD CLASS TO CATCH SIZE RESET IF/ELSE
+    setHieghts();
+    window.scrollTo({
+      top: window.innerHeight,
+      left: 0,
+      behavior: "smooth",
+    }); // FORCED SCROLL TO CONTACT SECTION
+    contactCard.classList.remove("move-card-back");
+    codeCard.classList.remove("move-card-back");
+    contactCard.classList.add("move-card-forward"); // ANIMATION TO MOVE UNUSED CARDS INTO VIEW AS CURRENT SELECTION CLOSES
+    codeCard.classList.add("move-card-forward");
+    designCard.classList.remove("theater");
+    setTimeout(() => {
+      aboutMeTitle.style.zIndex = 1;
+      document.body.style.overflow = "scroll"; // RESETS LOCKED SCROLL
+      document.body.style.overflowX = "hidden";
+      // REMOVES ANIMATION AS ANIMATION COMPLETES FORWARD
+      contactCard.classList.remove("move-card-forward");
+      codeCard.classList.remove("move-card-forward");
+      designCard.classList.remove("close-theater");
+    }, 1000);
+  }
 }
 
-openingContact();
+function handleCodeCloseBtn() {
+  if (codeCard.classList.contains("theater")) {
+    //CHECK IF ACTIVE CARD
+    codeCard.classList.add("close-theater"); // ADD CLASS TO CATCH SIZE RESET IF/ELSE
+    setHieghts();
+    window.scrollTo({
+      top: window.innerHeight,
+      left: 0,
+      behavior: "smooth",
+    }); // FORCED SCROLL TO CONTACT SECTION
+    contactCard.classList.remove("move-card-back");
+    designCard.classList.remove("move-card-back");
+    contactCard.classList.add("move-card-forward");
+    designCard.classList.add("move-card-forward");
+    codeCard.classList.remove("theater");
+    setTimeout(() => {
+      aboutMeTitle.style.zIndex = 1;
+      document.body.style.overflow = "scroll"; // RESETS LOCKED SCROLL
+      document.body.style.overflowX = "hidden";
+      // REMOVES ANIMATION AS ANIMATION COMPLETES FORWARD
+      contactCard.classList.remove("move-card-forward");
+      designCard.classList.remove("move-card-forward");
+      codeCard.classList.remove("close-theater");
+    }, 1000);
+  }
+}
+
+designCloseBtn.addEventListener("click", handleDesignCloseBtn);
+codeCloseBtn.addEventListener("click", handleCodeCloseBtn);
+
+function handleContactActiveDesignCard() {
+  if (contactCard.classList.contains("active-contact")) {
+    designLink.style.opacity = 0;
+    codeLink.style.opacity = 0;
+    handleContact();
+    setTimeout(() => {
+      designLink.style.opacity = 1;
+      codeLink.style.opacity = 1;
+    }, 1000);
+  }
+
+  if (!this.classList.contains("close-theater")) {
+    this.classList.add("theater"); // MAKE CARD ACTIVE - FRONT AND CENTER
+  }
+
+  this.classList.add("curtain-closed"); // MAKES OTHER ELEMENTS UNCLICKABLE DURING TRANSITION
+
+  if (designCard.classList.contains("theater")) {
+    aboutMeTitle.style.zIndex = -1;
+    designCard.style.height = null; // WIPES ELEMENT.STYLE SETTINGS
+    designCard.style.width = null;
+    designCard.style.left = null;
+    designCard.style.top = null;
+    contactCard.classList.add("move-card-back"); // ANIMATION TO MOVE UNUSED CARDS OUT OF VIEW
+    codeCard.classList.add("move-card-back");
+    setScrollOnOpenTheater();
+  }
+  setTimeout(() => {
+    designCard.classList.remove("curtain-closed");
+    contactCard.classList.remove("curtain-closed");
+  }, 1000);
+}
+function handleContactActiveCodeCard() {
+  if (contactCard.classList.contains("active-contact")) {
+    designLink.style.opacity = 0;
+    codeLink.style.opacity = 0;
+    handleContact();
+    setTimeout(() => {
+      designLink.style.opacity = 1;
+      codeLink.style.opacity = 1;
+    }, 1000);
+  }
+
+  if (!this.classList.contains("close-theater")) {
+    this.classList.add("theater"); // MAKE CARD ACTIVE - FRONT AND CENTER
+  }
+
+  this.classList.add("curtain-closed"); // MAKES OTHER ELEMENTS UNCLICKABLE DURING TRANSITION
+
+  if (codeCard.classList.contains("theater")) {
+    aboutMeTitle.style.zIndex = -1;
+    // REPEATS SAME MOTIONS BUT FOR CODE CARD
+    codeCard.style.height = null; // WIPES ELEMENT.STYLE SETTINGS
+    codeCard.style.width = null;
+    codeCard.style.left = null;
+    codeCard.style.top = null;
+    contactCard.classList.add("move-card-back");
+    designCard.classList.add("move-card-back");
+    setScrollOnOpenTheater();
+  }
+
+  // REMOVES UNCLICKABLE CLASS
+  setTimeout(() => {
+    this.classList.remove("curtain-closed");
+    contactCard.classList.remove("curtain-closed");
+  }, 1000);
+}
+
+designCard.addEventListener("click", handleContactActiveDesignCard);
+codeCard.addEventListener("click", handleContactActiveCodeCard);
+
 // HANDLES CONTACT CARD LOGISTICS
 contactCard.addEventListener("click", handleContact);
 
@@ -145,7 +195,6 @@ function handleContact() {
     portfolioCont.style.transform = "translateY(-50vh)"; // LIFTS UP PROTFOLIO SECTION TO GIVE FEEL OF MOVEMENT
   }
 }
-
 
 function setScrollOnOpenTheater() {
   document.body.style.overflow = "hidden";
