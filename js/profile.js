671776670a77831c95acb77191cf1400ed6853cf
Austin Olijar <Austin.Olijar@gmail.com>
2021-12-22 16:00:01 -0600

    Add files via upload

--- js/profile.js
+++ js/profile.js
@@ -289,10 +289,15 @@ function handleContact() {
 }
 // SCROLL RESET FUNCTION
 function setScrollOnOpenTheater() {
-  
-    document.body.style.overflow = "hidden";
-    window.scrollTo({ top: window.innerHeight, left: 0, behavior: "smooth" }); // FORCED SCROLL TO CONTACT SECTION
- 
+    if (window.innerWidth > 750) {
+      document.body.style.overflow = "hidden";
+      window.scrollTo({ top: window.innerHeight, left: 0, behavior: "smooth" }); // FORCED SCROLL TO CONTACT SECTION  
+    } 
+    else 
+    {
+      window.scrollTo({ top: window.innerHeight, left: 0, behavior: "smooth" }); // FORCED SCROLL TO CONTACT SECTION  
+    }
+
 }
 // CLOSE BTN EVENTS
 designCloseBtn.addEventListener("click", handleDesignCloseBtn);
