7c97bd56e386f9f70261beab4d85491853c00e02
Austin Olijar <Austin.Olijar@gmail.com>
2022-01-10 13:29:55 -0600

    Add files via upload

--- js/textPush.js
+++ js/textPush.js
@@ -2,6 +2,7 @@ const hoverTestText = document.querySelectorAll(".test");
 const hoverTestTextOff = document.querySelectorAll(".test-off");
 const hoverTestTextOn = document.querySelectorAll(".test-on");
 const moveDownArrow = document.getElementById("down-arrow-intro");
+const closeEmail = document.querySelector('.close-email-modal');
 
 hoverTestText.forEach((text) => {
   text.addEventListener("mouseenter", () => {
@@ -26,3 +27,6 @@ moveDownArrow.addEventListener('click', () => {
   }); 
 })
 
+closeEmail.addEventListener('click', () => {
+  emailModal.classList.remove("active-email-modal"); 
+})
